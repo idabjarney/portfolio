9dee11e01e8f3497c3b11ff1bb81cc359966ea87
idaism <ida.bjarney@outlook.com>
2023-09-14 15:02:28 +0100

    Finished contact page with a click to copy input for email address

--- contact/contact.ts
+++ contact/contact.ts
@@ -1,3 +1,17 @@
 import '../src/body.scss';
 import '../src/nav.scss';
 import '../src/variables.scss';
+
+const emailTextInput : HTMLInputElement | null = document.querySelector('.email-text-input');
+const copyEmailBtn : HTMLButtonElement | null = document.querySelector('.copy-email-btn');
+
+copyEmailBtn?.addEventListener('click', function(): void {
+  // console.log('hello')
+  emailTextInput?.select();
+  document.execCommand('copy');
+  copyEmailBtn.classList.add('active');
+  window.getSelection()?.removeAllRanges();
+  setTimeout(function() {
+    copyEmailBtn?.classList.remove('active');
+  }, 1500);
+});
